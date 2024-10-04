97a554415d9e8163d21ed38beaa12451557497fd
RailaAli <rahilaali895@gmail.com>
2024-10-04 16:09:16 +0500

    Second commit

---
+++ js.folder/main.js
@@ -0,0 +1,7 @@
+var typed= new typed(".text",{
+    Strings:["web developer" , "Programer" , "AI Researcher" , "Mathematican"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+});
